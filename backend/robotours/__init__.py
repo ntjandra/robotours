2d63f44c0d2460e1f2b6684cc9a64b0d1530012f
Nathaniel Tjandra <ntjandra@ucsc.edu>
2020-02-15 18:18:56 -0800

    Added filters for location/poi

--- backend/robotours/__init__.py
+++ backend/robotours/__init__.py
@@ -1,17 +1,20 @@
 import os
 from flask import Flask
+'''
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ROBOT_KEY'
+'''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['AMADEUS_KEY'] = 'API_TRAVEL_KEY' # Change this line in production. 
 # Maybe store?
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
+'''
 # Set these from OS environment variables
-from robotours import routes
+from robotours import server
--- backend/robotours/server.py
+++ backend/robotours/server.py
@@ -1,5 +1,7 @@
-import requests
-
+from flask import jsonify
+import random
+import requests, logging
+from robotours import app
 from amadeus import Client, ResponseError
 
 # Amadeus API Requirements
@@ -8,24 +10,59 @@ amadeus = Client(
     client_id='5E7BaLL3RjaHbAPgGCvQWk9iGa9pMdZJ',
     client_secret='MiBY6N6jH3TojvkF'
 )
-
+'''
 try:
     response = amadeus.reference_data.urls.checkin_links.get(airlineCode='BA')
     print(response.data)
 
 except ResponseError as error:
     print(error)
-
+'''
 # amadeus = Client() # Requires setup for enviornment variables
 
-@app.route('/location/interest/<int:lat>/<int:long>')
-def find_sighting(lat, long) {
+@app.route('/location/interest/<int:lat>/<int:long>/<string:filter>', methods=['GET'])
+def pick_poi(lat, long, filter='Any'):
     # Testing POI
     # Replace hardcode with varibles, when done.
-    result = amadeus.reference_data.locations.points_of_interest.get(latitude=41.397158, longitude=2.160873)
-    print(result)
-    return result;
-}
+    response = amadeus.reference_data.locations.points_of_interest.get(latitude=41.397158, longitude=2.160873)
+    '''
+    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    logging.debug("BODY")
+    logging.debug(response.body)
+    logging.debug("Data")
+    logging.debug(response.data)
+    logging.debug("Result")
+    logging.debug(response.result)
+    '''
+    json_response = jsonify(response.data)
+
+    # Parse the result into a list
+    list_response = response.data
+
+    # Filter Location results by Category
+    # Assumption: There are only two categories.
+    attractions = []
+    restaurants = []
+
+    for location in list_response:
+        print(location['category'])
+        # print(type(location['category']))
+        if location['category'] == "SIGHTS":
+            # print("Found an attraction!")
+            attractions.append(location)
+        else:
+            restaurants.append(location)
+
+    # Based on the filter, return
+    if filter == 'tour':
+        return jsonify(attractions)
+    elif filter == 'food':
+        return jsonify(restaurants)
+    else:
+        return json_response
+    list = response.result
+    location = random.choice(list)
+    return jsonify(location)
 
 if __name__ == '__main__':
     app.run()
