4b3d20496f4c901383a52b21d6d026da5eb018d7
FruitPunchKen <kenneth94024@gmail.com>
2020-02-15 18:42:49 -0800

    Finished getting current location. Im garbage so it took longer than expected.

--- robotour/app/src/main/java/com/example/robotour/MainActivity.java
+++ robotour/app/src/main/java/com/example/robotour/MainActivity.java
@@ -3,19 +3,58 @@ package com.example.robotour;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
+import android.content.Context;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    protected LocationManager locationManager;
+    private Location location;
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged (Location location){
+        }
+
+        @Override
+        public void onProviderDisabled (String provider){
+
+        }
+
+        @Override
+        public void onProviderEnabled (String provider){
+
+        }
+
+        @Override
+        public void onStatusChanged (String provider,int status, Bundle extras){
+
+        }
+    };
     private SearchView searchView;
     private Button btn;
+
+    // flag for GPS status
+    public boolean isGPSEnabled = false;
+
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    private long LOCATION_REFRESH_TIME = 10000;// REFERESH TIME EXPRESSED IN MILLISECONDS
+
+    private float LOCATION_REFRESH_DISTANCE = 10; // METERS
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +63,32 @@ public class MainActivity extends AppCompatActivity {
         searchView = findViewById(R.id.MainActivity_srcview_location);
         btn = findViewById(R.id.MainActivity_btn_useCurrentLocation);
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        }
+        if (checkLocationPermission()) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                    LOCATION_REFRESH_DISTANCE, locationListener);
+        }
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                getsLocation();
-                startActivity(new Intent(MainActivity.this, Destination.class));
+                if(getLocation())
+                {
+                    if(checkLocationPermission())
+                    {
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        Toast.makeText(MainActivity.this,"Location" + location.getLongitude() + "" +
+                                location.getLatitude(),Toast.LENGTH_SHORT).show();
+                    }
+                    Intent intent = new Intent(MainActivity.this, Destination.class);
+                    intent.putExtra("Longitude", location.getLongitude());
+                    intent.putExtra("Latitude", location.getLatitude());
+                    startActivity(intent);
+                }
             }
         });
         searchView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -55,10 +115,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void getsLocation()
-    {
-
+    public boolean checkLocationPermission() {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = this.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
     }
+    public boolean getLocation() {
+        // getting GPS status
+        isGPSEnabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+
+        // getting network status
+        isNetworkEnabled = locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if(isGPSEnabled && isNetworkEnabled &&internetIsConnected())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public boolean internetIsConnected() {
+        try {
+            String command = "ping -c 1 google.com";
+            return (Runtime.getRuntime().exec(command).waitFor() == 0);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     public void hideKeyboard(View view) {
         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
