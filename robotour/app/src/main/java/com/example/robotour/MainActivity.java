bc294fb15cebc1afc8def1840e38512ac5295f5c
henry dang <hodang@ucsc.edu>
2020-02-16 06:35:54 -0800

    Got permission on response to compile

--- robotour/app/src/main/java/com/example/robotour/MainActivity.java
+++ robotour/app/src/main/java/com/example/robotour/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.robotour;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.here.sdk.core.GeoCoordinates;
 import com.here.sdk.mapviewlite.MapScene;
 import com.here.sdk.mapviewlite.MapStyle;
 import com.here.sdk.mapviewlite.MapViewLite;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private PermissionsRequestor permissionsRequestor;
     private MapViewLite mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +32,76 @@ public class MainActivity extends AppCompatActivity {
         // Get a MapViewLite instance from the layout.
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
-        loadMapScene();
+       // handleAndroidPermissions();
+
+        loadMapScene(52.530932, 13.384915);
+
     }
 
-    private void loadMapScene() {
+    private void handleAndroidPermissions() {
+        permissionsRequestor = new PermissionsRequestor(this);
+        permissionsRequestor.request(new PermissionsRequestor.ResultListener(){
+
+            @Override
+            public void permissionsGranted() {
+                loadMapScene(36.2674,122.0147);
+            }
+
+            @Override
+            public void permissionsDenied() {
+                Log.e(TAG, "Permissions denied by user.");
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsRequestor.onRequestPermissionsResult(requestCode, grantResults);
+    }
+
+    private void createRequestPOI(int lat, int lng, String filter) {
+
+        String API;
+
+        {
+            API = "http://localhost:5000/location/interest/"
+                    + lat + "/" + lng + "/" + filter;
+        }
+
+        System.out.print(API);
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                API,
+                null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest API Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest API Response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+    }
+
+    private void loadMapScene(double lat,double lng) {
         // Load a scene from the SDK to render the map with a map style.
+//        double lat1 = 52.530932;
+//        double lng1 = 13.384915;
+
         mapView.getMapScene().loadScene(MapStyle.NORMAL_DAY, new MapScene.LoadSceneCallback() {
             @Override
             public void onLoadScene(@Nullable MapScene.ErrorCode errorCode) {
                 if (errorCode == null) {
-                    mapView.getCamera().setTarget(new GeoCoordinates(52.530932, 13.384915));
+                    mapView.getCamera().setTarget(new GeoCoordinates(lat, lng));
                     mapView.getCamera().setZoomLevel(14);
                 } else {
                     Log.d(TAG, "onLoadScene failed: " + errorCode.toString());
